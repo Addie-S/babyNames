1e1c734f093e7501b25e771b78f6a3b6a25cb3ce
Addie <adi1sanc@gmail.com>
2021-05-03 12:55:35 -0700

    Created Method "getRank"

--- printNames.java
+++ printNames.java
@@ -25,23 +25,64 @@ public class printNames {
         int totalBirths = 0; 
         int totalBoys = 0; 
         int totalGirls = 0; 
+        int totalNames = 0; 
+ 
         for (CSVRecord rec : fr.getCSVParser(false)) {
             int numBorn = Integer.parseInt(rec.get(2)); 
             totalBirths += numBorn; 
             if (rec.get(1).equals("M")) {
-                totalBoys += numBorn;               
+                totalBoys += numBorn;      
+                totalNames++; 
             } else {
-                totalGirls += numBorn;                
+                totalGirls += numBorn;  
+                totalNames++; 
             }
         }        
         System.out.println("Total births = " + totalBirths); 
         System.out.println("Total Girls = " + totalGirls); 
         System.out.println("Total Boys = " + totalBoys); 
+        System.out.println("Total names = " + totalNames); 
     }
     
     
     public void testTotalBirths() {
-        FileResource fr = new FileResource(); 
+        FileResource fr = new FileResource("/babyNames/data/example-small.csv"); 
         totalBirths(fr);                 
+    }
+    
+    public int getRank (int year, String name, String gender) {
+        int rank = 0; 
+        int position = 0; 
+        //"E:\babyNames\data\example-small.csv"
+        FileResource fr = new FileResource("/babyNames/data/yob" + year + "short.csv"); 
+        CSVParser parser = fr.getCSVParser(false); 
+        for (CSVRecord rec : parser) {
+             
+            String nameFound = rec.get(0); 
+            String genderFound = rec.get(1); 
+                                             
+            if (genderFound.equals(gender)) { 
+                ++rank;
+                if (nameFound.equals(name)) {
+                rank = rank; 
+                break; 
+                }
+            }                                    
+        }
+        
+        if (rank == 0) {
+                return -1; 
+        }         
+            
+        return rank; 
+    }
+    
+    public void testGetRank() {
+        //FileResource fr = new FileResource(); 
+        System.out.println(getRank(2014, "Ava", "F")); 
+        System.out.println(getRank(2014, "William", "M")); 
+        System.out.println(getRank(2014, "Emma", "F")); 
+        System.out.println(getRank(2014, "Noah", "M"));
+        System.out.println(getRank(2014, "Mason", "M")); 
     }
 }
